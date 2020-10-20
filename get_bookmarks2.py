49dad1e0a9a862c19d103d34ba1d0d4639156685
Linkeer365 <564142445@qq.com>
2020-10-20 21:12:09 +0800

    2020年10月20日21:12:01 linux版本是2.py 这个可以跑在服务器上

---
+++ get_bookmarks2.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+import subprocess
+
+from upload2ArchiveOrg2 import get_links
+
+# import requests
+
+bookmarks_path="/root/ArchiveMePlease/Bookmarks"
+bookmarks_save_path="/root/ArchiveMePlease/bookmarks.txt"
+
+if os.path.exists(bookmarks_save_path):
+	with open(bookmarks_save_path,"r",encoding="utf-8") as f:
+		old_bookmarks=[each.strip("\n") for each in f.readlines()]
+		old_bookmarks_set=set(old_bookmarks)
+
+folder_name="ArchiveMePlease!"
+links=get_links(folder_name)
+
+links=[each for each in links if not each in old_bookmarks_set]
+
+links_s="\n".join(links)
+
+with open(bookmarks_save_path,"a",encoding="utf-8") as f:
+	f.write(links_s)
+	f.write("\n")
+
+print("written done.")
+
